097d7ed2468e93613c07923979f78b2f79581d7d
Tomasz Idziak <tomasz.idziak@gmail.com>
2014-05-24 10:55:32 +0200

    Zmiany

--- src/main/java/com/translator/Main.java
+++ src/main/java/com/translator/Main.java
@@ -1,6 +1,19 @@
 
 package com.translator;
+import com.translator.parser.JavaLexer;
+import com.translator.parser.JavaParser;
+import java.io.IOException;
+import java.io.InputStream;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        InputStream stream = Main.class.getResourceAsStream("JavaSourceCode.j");
+        ANTLRInputStream antlrStream = new ANTLRInputStream(stream);
+        JavaLexer lexer = new JavaLexer(antlrStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        JavaParser parser = new JavaParser(tokens);
 
+    }
 }
