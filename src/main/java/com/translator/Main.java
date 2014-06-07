58ec8b617e4b191cc71bfe5c578bbc8d43850f30
m-materniak <maternia@student.agh.edu.pl>
2014-06-07 16:01:19 +0200

    gui form

--- src/main/java/com/translator/Main.java
+++ src/main/java/com/translator/Main.java
@@ -1,28 +1,12 @@
 
 package com.translator;
-import com.translator.parser.JavaLexer;
-import com.translator.parser.JavaParser;
-import com.translator.parser.SimpleListener;
-import com.translator.structure.MainMethodDeclaration;
+import com.translator.gui.MainWindow;
 import java.io.IOException;
-import java.io.InputStream;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        InputStream stream = Main.class.getResourceAsStream("JavaSourceCode.j");
-        ANTLRInputStream antlrStream = new ANTLRInputStream(stream);
-        JavaLexer lexer = new JavaLexer(antlrStream);
-        CommonTokenStream tokens = new CommonTokenStream(lexer);
-        JavaParser parser = new JavaParser(tokens);
-
-        ParserRuleContext tree = parser.compilationUnit();
-        ParseTreeWalker walker = new ParseTreeWalker();
-        SimpleListener listener = new SimpleListener();
-        walker.walk(listener, tree);
+        MainWindow window = new MainWindow();
+        window.setVisible(true);
     }
 
 }
