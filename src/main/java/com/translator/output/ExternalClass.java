fce70caf1ef40d632cf6a3e9003dac13b03516df
Tomasz Idziak <tomasz.idziak@gmail.com>
2014-06-08 20:04:13 +0200

    Przed tworzeniem wrapperów

---
+++ src/main/java/com/translator/output/ExternalClass.java
@@ -0,0 +1,15 @@
+
+package com.translator.output;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class ExternalClass {
+
+    private String name;
+    private final List<Method> methods = new LinkedList<>();
+
+    public ExternalClass(String name) {
+
+    }
+}
---
+++ src/main/java/com/translator/output/Method.java
@@ -0,0 +1,13 @@
+
+package com.translator.output;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Method {
+    public List<Argument> arguments = new LinkedList<>();
+
+    public class Argument {
+        private String type;
+    }
+}
